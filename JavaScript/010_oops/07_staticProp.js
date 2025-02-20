1540be0bcf41ef545738b1a0dbb144ec89f525a3
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2025-02-20 10:28:49 +0530

    inheritance and static property of class

---
+++ JavaScript/010_oops/07_staticProp.js
@@ -0,0 +1,26 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+
+    static createId(){  // will not be accessible to child
+        return `123`
+    }
+}
+
+const rishit = new User("rishit")
+// console.log(rishit.createId())
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+console.log(iphone.createId());
