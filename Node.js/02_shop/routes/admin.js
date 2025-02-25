afe8e31a1782e48b1e1f5a44ef260000afe1b84b
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2025-02-25 11:00:07 +0530

    managed rendering for different view engines

--- Node.js/02_shop/routes/admin.js
+++ Node.js/02_shop/routes/admin.js
@@ -3,14 +3,20 @@ const path = require("path");
 
 const router = express.Router();
 
+const products = []
+
 router.get("/add-product", (req, res) => {
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html")); // send response
+  // res.sendFile(path.join(__dirname, "../", "views", "add-product.html")); // send response
+  res.render('add-product', {pageTitle: 'Add Product'})
 });
 
-router.post("/product", (req, res) => {
+router.post("/add-product", (req, res) => {
   ///will only execute for post request
-  console.log(req.body);
+  const {name, price, desc} = req.body
+  products.push({name, price, desc})
   res.redirect("/");
 });
 
-module.exports = router;
+// module.exports = router;
+exports.routes = router
+exports.products = products
--- Node.js/02_shop/routes/shop.js
+++ Node.js/02_shop/routes/shop.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const path = require('path')
 const routeDir = require('../util/path')
+const adminData = require('./admin')
 
 const app = express()
 
 app.get('/', (req, res, next) => {
-    res.sendFile(path.join(routeDir, 'views', 'shop.html')) // send response
+    // console.log(adminData.products)
+    // res.sendFile(path.join(routeDir, 'views', 'shop.html')) // send response
+
+    const products = adminData.products
+    res.render('shop', {prods: products, pageTitle: 'Shop'}) // pug file
 })
 
 module.exports = app
