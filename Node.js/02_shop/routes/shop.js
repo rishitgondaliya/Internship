8ca4728e2ac5f440b63ce24abb8a5dbedf290501
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2025-02-26 10:21:30 +0530

    modify routes for controllers

--- Node.js/02_shop/routes/shop.js
+++ Node.js/02_shop/routes/shop.js
@@ -1,16 +1,35 @@
-const express = require('express')
-const path = require('path')
-const routeDir = require('../util/path')
-const adminData = require('./admin')
+// const express = require("express");
+// const path = require("path");
+// const routeDir = require("../util/path");
+// const adminData = require("./admin");
 
-const app = express()
+// const app = express();
 
-app.get('/', (req, res, next) => {
-    // console.log(adminData.products)
-    // res.sendFile(path.join(routeDir, 'views', 'shop.html')) // send response
+// app.get("/", (req, res, next) => {
+//   console.log(adminData.products)
+//   res.sendFile(path.join(routeDir, 'views', 'shop.html')) // send response
 
-    const products = adminData.products
-    res.render('shop', {prods: products, pageTitle: 'Shop'}) // pug file
-})
+//   const products = adminData.products;
+//   res.render("shop", { prods: products, pageTitle: "Shop" }); // pug file
+// });
 
-module.exports = app
+// module.exports = app;
+
+// ************** Using controllers ***************
+
+const express = require("express");
+const path = require("path");
+
+const shopController = require("../controllers/shop_controller");
+
+const app = express();
+
+app.get("/", shopController.getIndex);
+
+app.get('/products', shopController.getProducts)
+
+app.get('/cart', shopController.getCart)
+
+app.get('/checkout', shopController.getCheckout)
+
+module.exports = app;
