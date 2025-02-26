bafbf8c81e8e41e68449516186b702523f458ce7
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2025-02-26 10:19:45 +0530

    modify routes for controllers

--- Node.js/02_shop/app.js
+++ Node.js/02_shop/app.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
-const handleBar = require("express-handlebars");
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const path = require("path");
+// const handleBar = require("express-handlebars");
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+// const adminRoutes = require("./routes/admin");
+// const shopRoutes = require("./routes/shop");
 
-const app = express();
+// const app = express();
 // const router = express.Router() // will work exactly same ass app
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.static(path.join(__dirname, "public")));
 
 // hbs
 // app.engine(
@@ -27,21 +27,51 @@ app.use(express.static(path.join(__dirname, "public")));
 // app.set('view engine', 'pug') // set view engine
 
 // ejs
-app.set("view engine", "ejs");
+// app.set("view engine", "ejs");
 
-app.set("views", "views");
+// app.set("views", "views");
 
 // app.use((req, res, next) => {
 //     console.log('In the middleware!')
 //     next() // call the next middleware
 // })
 
-app.use(adminRoutes.routes);
+// app.use(adminRoutes.routes);
+// app.use(shopRoutes);
+
+// app.use((req, res) => {
+//   res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+//   res.status(404).render("404", { pageTitle: "Page not found" });
+// });
+
+// app.listen(3000); //satrt the server
+
+
+
+// *************** Using controller *****************
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+const errorController = require('./controllers/error404')
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// ejs
+app.set("view engine", "ejs");
+
+app.set("views", "views");
+
+app.use(adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res) => {
-  // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-  res.status(404).render("404", { pageTitle: "Page not found" });
-});
+app.use(errorController.getError);
 
-app.listen(3000); //satrt the server
+app.listen(3000);
