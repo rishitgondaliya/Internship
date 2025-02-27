ccae6609b13b088d12ea213626483dcc2407be64
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2025-02-27 10:19:52 +0530

    added find product function

--- Node.js/02_shop/models/product.js
+++ Node.js/02_shop/models/product.js
@@ -20,7 +20,6 @@
 //     }
 // }
 
-
 // store data in a file using model
 
 const fs = require("fs");
@@ -42,22 +41,43 @@ const getProductsFromFile = (callback) => {
 };
 
 module.exports = class Product {
-  constructor(name, price, desc) {
+  constructor(id, name, imgUrl, price, desc) {
+    this.id = id;
     this.name = name;
+    this.imgUrl = imgUrl;
     this.price = price;
     this.desc = desc;
   }
 
   save() {
     getProductsFromFile((products) => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log(err);
-      });
+      if (this.id) {
+        const existingProductIndex = products.findIndex(
+          (prod) => prod.id === this.id
+        );
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+          console.log(err);
+        });
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+          console.log(err);
+        });
+      }
     });
   }
 
   static fetchAll(callback) {
     getProductsFromFile(callback);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile((products) => {
+      const product = products.find((p) => p.id === id);
+      cb(product);
+    });
   }
 };
