9d7c57ab4b5dd5ec76e00bef4726073c855434f6
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2025-02-27 10:12:52 +0530

    added controller functions for editing product

--- Node.js/02_shop/controllers/admin_controller.js
+++ Node.js/02_shop/controllers/admin_controller.js
@@ -1,25 +1,60 @@
-const Product = require('../models/product')
+const Product = require("../models/product");
 
 // add-product controller
 
 exports.getAddProduct = (req, res) => {
   res.render("admin/add-product", {
     pageTitle: "Add Product",
+    path: "/admin/add-product",
+    isEditing: false,
   });
 };
 
 exports.postAddProduct = (req, res) => {
-  const {name, price, desc} = req.body
-  const product = new Product(name, price, desc)
-  product.save()
-  res.redirect('/')
-} 
+  const { name, imgUrl, price, desc } = req.body;
+  const product = new Product(null, name, imgUrl, price, desc);
+  product.save();
+  res.redirect("/");
+};
 
 exports.getProducts = (req, res) => {
-  Product.fetchAll(products => {
+  Product.fetchAll((products) => {
     res.render("admin/products", {
       prods: products,
       pageTitle: "Admin Products",
-    });  
-  })
-}
+      path: "/admin/products",
+    });
+  });
+};
+
+exports.getEditProduct = (req, res) => {
+  const prodId = req.params.productId;
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect("/");
+  }
+  Product.findById(prodId, (product) => {
+    if (!product) {
+      res.redirect("/");
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      isEditing: editMode,
+      product: product,
+    });
+  });
+};
+
+exports.postEditProduct = (req, res) => {
+  const prodId = req.body.productId;
+  const { updatedName, updatedPrice, updatedImgUrl, updatedDesc } = req.body;
+  const updatedProduct = new Product(
+    prodId,
+    updatedName,
+    updatedPrice,
+    updatedImgUrl,
+    updatedDesc
+  );
+  updatedProduct.save();
+};
