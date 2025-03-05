f16d246bb5e6cb869c844e06e3cc147d960e7ff1
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2025-03-05 10:12:50 +0530

    adding cart items to order and making cart empty

--- Node.js/02_shop/controllers/shop_controller.js
+++ Node.js/02_shop/controllers/shop_controller.js
@@ -289,39 +289,100 @@ exports.getOrders = (req, res) => {
 };
 
 // create order
-exports.postOrder = (req, res) => {
-  let fetchedCart;
-  req.user
-    .getCart()
-    .then((cart) => {
-      fetchedCart = cart;
-      return cart.getProducts();
-    })
-    .then((products) => {
-      req.user
-        .createOrder()
-        .then((order) => {
-          return order.addProducts(
-            products.map((product) => {
-              product.orderItem = { quantity: product.cartItem.quantity };
-              return product;
-            })
-          );
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    })
-    .then((result) => {
-      fetchedCart.setProducts(null);
-    })
-    .then((result) => {
-      res.redirect("/orders");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+exports.postOrder = async (req, res) => {
+  try {
+    const cart = await req.user.getCart();
+    const products = await cart.getProducts();
+
+    const order = await req.user.createOrder();
+    await order.addProducts(
+      products.map((product) => {
+        product.orderItem = { quantity: product.cartItem.quantity };
+        return product;
+      })
+    );
+
+    await cart.setProducts(null);
+
+    res.redirect("/orders");
+  } catch (err) {
+    console.error("Error while creating order:", err);
+    res.status(500).send("Something went wrong!");
+  }
 };
+
+// exports.postOrder = (req, res) => {
+//   let fetchedCart;
+//   req.user
+//     .getCart()
+//     .then((cart) => {
+//       fetchedCart = cart;
+//       return cart.getProducts();
+//     })
+//     .then((products) => {
+//       req.user
+//         .createOrder()
+//         .then((order) => {
+//           return order.addProducts(
+//             products.map((product) => {
+//               product.orderItem = { quantity: product.cartItem.quantity };
+//               return product;
+//             })
+//           );
+//         })
+//         .catch((err) => {
+//           console.log(err);
+//         });
+//     })
+//     .then((result) => {
+//       fetchedCart.setProducts(null);
+//     })
+//     .then((result) => {
+//       res.redirect("/orders");
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// };
+
+
+// delete a product from order
+// exports.postDeleteOrderItem = async (req, res) => {
+//   try {
+//     const prodId = req.body.productId;
+
+//     // Find the user's orders that contain the given product
+//     const orders = await req.user.getOrders({
+//       include: [{ model: Product, where: { id: prodId } }]
+//     });
+
+//     if (!orders.length) {
+//       console.log("No matching order found for product:", prodId);
+//       return res.redirect("/orders");
+//     }
+
+//     for (let order of orders) {
+//       const products = await order.getProducts({ where: { id: prodId } });
+
+//       if (products.length > 0) {
+//         // Delete the order item entry from 'order-items' table
+//         await order.removeProduct(products[0]); 
+
+//         // Check if there are remaining products in this order
+//         const remainingProducts = await order.getProducts();
+//         if (remainingProducts.length === 0) {
+//           await order.destroy(); // Delete order if no products remain
+//         }
+//       }
+//     }
+
+//     res.redirect("/orders"); // Redirect after deletion
+//   } catch (err) {
+//     console.error("Error while canceling order:", err);
+//     res.status(500).send("Error while canceling order");
+//   }
+// };
+
 
 // checkout page
 exports.getCheckout = (req, res) => {
