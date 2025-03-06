41873a6c982cf54aeac91434bb1e4cd43255c240
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2025-03-06 10:27:00 +0530

    revison

---
+++ JavaScript/01_basics/08_operators.js
@@ -0,0 +1,11 @@
+// arithmetic
+// assignment
+// comparison
+// logical -> &&, `(logical OR), !
+// bitwise -> &, ^(XOR), |, `, ~(NOT), <<, >>
+// ternary
+// type -> typeof, instanceof
+// spread ... & rest
+// delete -> delete property of an object
+// new -> create an instance of an object.
+// in -> returns true if the specified property is in the specified object
