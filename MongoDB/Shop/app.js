1ff90a79a3b9238b96bee60cdc5c8aa72f8601ca
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2025-03-07 10:05:02 +0530

    app.js and packages

---
+++ MongoDB/Shop/app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const errorController = require("./controllers/error404");
+const mongoConnect = require("./util/database").mongoConnect;
+const User = require("./models/user");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// ejs
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use((req, res, next) => {
+  User.findById("67c83a44a112e80474f289df")
+    .then((user) => {
+      req.user = new User(user.userName, user.email, user.cart, user._id);
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.getError);
+
+mongoConnect(() => {
+  // console.log(getDb());
+  app.listen(3000, () => {
+    console.log("server is running on http://localhost:3000");
+  });
+});
