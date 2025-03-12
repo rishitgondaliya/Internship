b3745f7cb9c66b078248ce2ddec7d16249cbe9ca
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2025-03-12 10:34:40 +0530

    added CSRF protection, managed creating user, set local variables

--- MongoDB/Mongoose/Shop/app.js
+++ MongoDB/Mongoose/Shop/app.js
@@ -4,7 +4,8 @@ const path = require("path");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const session = require("express-session");
-const MongoDBStore = require('connect-mongodb-session')(session)
+const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require('csurf')
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -17,8 +18,9 @@ dotenv.config();
 const app = express();
 const store = new MongoDBStore({
   uri: process.env.MONGO_DRIVER_URL,
-  collection: 'sessions',
-})
+  collection: "sessions",
+});
+const csrfProtection = csrf()
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -30,21 +32,43 @@ app.use(
     store: store,
   })
 );
+app.use(csrfProtection)
 
 // ejs
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+// app.use((req, res, next) => {
+//   User.findById("67ca9bbf2d0bf9ca7b9d58e2")
+//     .then((user) => {
+//       req.user = user;
+//       next();
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
+
+// when using session
 app.use((req, res, next) => {
-  User.findById("67ca9bbf2d0bf9ca7b9d58e2")
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then((user) => {
-      req.session.user = user;
+      req.user = user;
       next();
     })
     .catch((err) => {
       console.log(err);
     });
 });
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+})
 
 app.use(adminRoutes);
 app.use(shopRoutes);
@@ -56,18 +80,20 @@ mongoose
   .connect(process.env.MONGO_DRIVER_URL)
   .then((result) => {
     console.log("Connected established.!");
-    User.findOne().then((user) => {
-      if (!user) {
-        const user = new User({
-          name: "Rishit",
-          email: "r@test.com",
-          cart: {
-            items: [],
-          },
-        });
-        user.save(); // save() -> rovided by default by mongoose
-      }
-    });
+    // we don't need to create user here because we have implemented authentication
+
+    // User.findOne().then((user) => {
+    //   if (!user) {
+    //     const user = new User({
+    //       name: "Rishit", 
+    //       email: "r@test.com",
+    //       cart: {
+    //         items: [],
+    //       },
+    //     });
+    //     user.save(); // save() -> rovided by default by mongoose
+    //   }
+    // });
     app.listen(3000, () => {
       console.log("server is running on http://localhost:3000");
     });
