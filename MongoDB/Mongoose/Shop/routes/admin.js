27105c93af76949a9e91cbf43580ef46c0993a87
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2025-03-12 10:32:05 +0530

    securing routes

--- MongoDB/Mongoose/Shop/routes/admin.js
+++ MongoDB/Mongoose/Shop/routes/admin.js
@@ -1,19 +1,21 @@
 const express = require("express");
 
 const adminController = require("../controllers/admin_controller");
+const isAuth = require('../middleware/is_auth')
 
 const router = express.Router();
 
-router.get("/admin/add-product", adminController.getAddProduct);
+// controllers will be executed from left to right
+router.get("/admin/add-product", isAuth, adminController.getAddProduct);
 
-router.post("/admin/add-product", adminController.postAddProduct);
+router.post("/admin/add-product", isAuth, adminController.postAddProduct);
 
-router.get("/admin/products", adminController.getProducts);
+router.get("/admin/products", isAuth, adminController.getProducts);
 
-router.get('/admin/edit-product/:productId', adminController.getEditProduct)
+router.get('/admin/edit-product/:productId', isAuth, adminController.getEditProduct)
 
-router.post('/admin/edit-product', adminController.postEditProduct)
+router.post('/admin/edit-product', isAuth, adminController.postEditProduct)
 
-router.post('/admin/delete-product', adminController.postDeleteProduct)
+router.post('/admin/delete-product', isAuth, adminController.postDeleteProduct)
 
 module.exports = router;
--- MongoDB/Mongoose/Shop/routes/shop.js
+++ MongoDB/Mongoose/Shop/routes/shop.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const path = require("path");
 
 const shopController = require("../controllers/shop_controller");
+const isAuth = require('../middleware/is_auth')
 
 const app = express();
 
@@ -11,14 +11,14 @@ app.get('/products', shopController.getProducts)
 
 app.get('/products/:productId', shopController.getProduct)
 
-app.get('/cart', shopController.getCart)
+app.get('/cart', isAuth, shopController.getCart)
 
-app.post('/cart', shopController.postCart)
+app.post('/cart', isAuth, shopController.postCart)
 
-app.post('/delete-cart-item', shopController.postDeleteCartItem)
+app.post('/delete-cart-item', isAuth, shopController.postDeleteCartItem)
 
-app.get('/orders', shopController.getOrders)
+app.get('/orders', isAuth, shopController.getOrders)
 
-app.post('/create-order', shopController.postOrder)
+app.post('/create-order', isAuth, shopController.postOrder)
 
 module.exports = app;
