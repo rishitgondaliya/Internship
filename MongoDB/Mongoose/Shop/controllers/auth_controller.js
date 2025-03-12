0cd5b50e94f0401a3f0b1d000ae5287330c4187f
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2025-03-12 10:24:43 +0530

    added login & signup controller, implemented password hashing

--- MongoDB/Mongoose/Shop/controllers/auth_controller.js
+++ MongoDB/Mongoose/Shop/controllers/auth_controller.js
@@ -1,23 +1,71 @@
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+
+exports.getSignup = (req, res) => {
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Sign Up",
+    isAuthenticated: false,
+  });
+};
+
+exports.postSignup = (req, res) => {
+  const { name, email, password } = req.body;
+  User.findOne({ email: email })
+    .then((userDoc) => {
+      if (userDoc) {
+        // return res.status(422).json({message: 'Email already exists.'})
+        return res.redirect("/signup");
+      }
+      return bcrypt
+        .hash(password, 12)
+        .then((hashedPassword) => {
+          const user = new User({
+            name: name,
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] },
+          });
+          return user.save();
+        })
+        .then((result) => {
+          res.redirect("/login");
+        });
+    })
+    .catch((err) => console.log(err));
+};
 
 exports.getLogin = (req, res) => {
-  //   const isLoggedIn = req.get("Cookie");
-  console.log(req.session.isLoggedIn);
+  // const isLoggedIn = req.get("Cookie");
+  // console.log(req.session.isLoggedIn);
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
     isAuthenticated: false,
+    // csrfToken: req.csrfToken()
   });
 };
 
 exports.postLogin = (req, res) => {
   //   req.isLoggedIn = true;
   //   res.setHeader("Set-Cookie", "isLoggedIn = true; httpOnly"); // set cookie
-  User.findById("67ca9bbf2d0bf9ca7b9d58e2")
+  // User.findById("67ca9bbf2d0bf9ca7b9d58e2")
+  const { email, password } = req.body;
+  User.findOne({ email: email })
     .then((user) => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      res.redirect("/");
+      if (!user) {
+        return res.redirect("/login");
+      }
+      bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return res.redirect("/login");
+        }
+        req.session.isLoggedIn = true;
+        req.session.user = user;
+        req.session.save((err) => {
+          res.redirect("/");
+        });
+      });
     })
     .catch((err) => {
       console.log(err);
@@ -26,8 +74,6 @@ exports.postLogin = (req, res) => {
 
 exports.postLogout = (req, res) => {
   req.session.destroy((err) => {
-    console.log(err);
-    // res.clearCookie('isLoggedIn');
-    res.redirect("/");
+    res.redirect("/login");
   });
 };
