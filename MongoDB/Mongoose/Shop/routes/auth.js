60310545ab355a07aae763e527f15eae21c2c4a5
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2025-03-17 10:47:14 +0530

    added routes for resetting password

--- MongoDB/Mongoose/Shop/routes/auth.js
+++ MongoDB/Mongoose/Shop/routes/auth.js
@@ -14,4 +14,12 @@ router.post('/login', authController.postLogin)
 
 router.post('/logout', authController.postLogout)
 
+router.get('/reset', authController.getResetPassword)
+
+router.post('/reset', authController.postResetPassword)
+
+router.get('/reset/:token', authController.getNewPassword)
+
+router.post('/new-password', authController.postNewPassword)
+
 module.exports = router
