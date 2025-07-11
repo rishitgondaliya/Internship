81efc6d2e027d1ef5dd08650d53bf45e7123ac7c
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2025-07-11 09:46:56 +0530

    next api routes

---
+++ React+Next/next/api/pages/api/feedback.js
@@ -0,0 +1,34 @@
+import fs from "fs";
+import path from "path";
+
+export function buildFeedbackPath() {
+  return path.join(process.cwd(), "data", "feedback.json");
+}
+
+export function extractFeedback(filePath) {
+  const fileData = fs.readFileSync(filePath);
+  const data = JSON.parse(fileData);
+  return data;
+}
+
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    const email = req.body.email;
+    const feedbackText = req.body.text;
+
+    const newFeedback = {
+      email,
+      text: feedbackText,
+      id: new Date().toISOString(),
+    };
+    const filePath = buildFeedbackPath();
+    const data = extractFeedback(filePath);
+    data.push(newFeedback);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    res.status(200).json({ message: "Success!", feedback: newFeedback });
+  } else {
+    const filePath = buildFeedbackPath();
+    const data = extractFeedback(filePath);
+    res.status(200).json({ feedback: data });
+  }
+}
