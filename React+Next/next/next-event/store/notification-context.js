2e4270a531e3e41d5ec3c993905a9f611d395601
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2025-07-15 09:59:54 +0530

    using context api

---
+++ React+Next/next/next-event/store/notification-context.js
@@ -0,0 +1,56 @@
+import { createContext, useEffect, useState } from "react";
+
+const NotificationContext = createContext({
+  notification: null, // {title, message, status}
+  showNotification: function (data) {},
+  hideNotification: function () {},
+});
+
+export function NotificationContextProvider(props) {
+  const [activeNotification, setActiveNotification] = useState();
+
+  useEffect(() => {
+    if (
+      activeNotification &&
+      (activeNotification.status === "success" ||
+        activeNotification.status === "error")
+    ) {
+      const timer = setTimeout(() => {
+        setActiveNotification(null);
+      }, 3000);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [activeNotification]);
+
+  function showNotificationHandler(data) {
+    setActiveNotification(
+      {
+        title: data.title,
+        message: data.message,
+        status: data.status,
+      }
+      //   data
+    );
+  }
+
+  function hideNotificationHandler() {
+    setActiveNotification(null);
+  }
+
+  const context = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler,
+  };
+
+  return (
+    <NotificationContext.Provider value={context}>
+      {props.children}
+    </NotificationContext.Provider>
+  );
+}
+
+export default NotificationContext;
