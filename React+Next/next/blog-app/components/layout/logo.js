c12ed9765ff0e312af01b4927b615de1075932c9
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2025-07-18 10:05:31 +0530

    deleted

--- React+Next/next/blog-app/components/layout/logo.js
+++
@@ -1,5 +0,0 @@
-import classes from "./logo.module.css";
-
-export default function Logo() {
-  return <div className={classes.logo}>Rishit' Next Blog</div>;
-}
--- React+Next/next/blog-app/pages/contact.js
+++
@@ -1,5 +0,0 @@
-import ContactForm from "../components/contact/contact-form";
-
-export default function ContactPage() {
-  return <ContactForm />;
-}
